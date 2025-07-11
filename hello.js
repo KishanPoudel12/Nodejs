f6d5ef3b6bc3e6bceee26b1e0b3a84eec8f00485
kishan <kishanpoudel563@gmail.com>
2025-07-11 19:43:46 +0530

    globalThis is the new Global

---
+++ hello.js
@@ -0,0 +1,14 @@
+console.log("kishan")
+//REPL => Read , Evaluate , Print , Loop
+
+
+// just like the window for the  browser , there is global for the node 
+
+console.log(global )   // global is like windows for browser    , there are more like window (self , frames , this)
+console.log(this)  // This is an empty object here  
+
+
+// Dont worry , globalThis will work everyWhere :)
+
+
+console.log(globalThis)
